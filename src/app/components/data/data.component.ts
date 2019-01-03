9c6d4ceca39543cb7f6a1fe91d6f92377ad09abf
Andres <jhonatan.leon@farmatodo.com>
2019-01-03 13:50:12 -0500

    formulario con validacion por data, array de inputs

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray} from '@angular/forms';
 
 @Component({
   selector: 'app-data',
@@ -7,9 +8,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataComponent implements OnInit {
 
-  constructor() { }
+	forma:FormGroup;
 
-  ngOnInit() {
-  }
+	usuario:Object = {
+		nombrecompleto: {
+			nombre: 'andres',
+			apellido: 'leon'
+		},
+		correo:'andres@gmail.com',
+		//pasatiempos:["correr","dormir","jugar"]
+	}
+
+  	constructor() { 
+
+  		console.log("this.usuario",this.usuario)
+
+  		this.forma = new FormGroup({
+  			'nombrecompleto': new FormGroup({
+	  			'nombre': new FormControl('', [Validators.required, Validators.minLength(3)]),
+	  			'apellido': new FormControl('', Validators.required)  				
+  			}),
+  			'correo': new FormControl('', [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")]),
+  			'pasatiempos': new FormArray([
+  					new FormControl('Correr', Validators.required)
+  				])
+
+  		});
+
+  	}
+
+  	ngOnInit() {
+  	}
+
+  	guardarCambios(){
+  		console.log(this.forma.value);
+
+  		this.forma.reset({
+  			nombrecompleto:{
+  				nombre:'',
+  				apellido: ''
+  			},
+  			correo: ''
+  		})
+  	}
+
+  	agregarPasatiempo(){
+  		(<FormArray>this.forma.controls['pasatiempos']).push(
+  			new FormControl('', Validators.required )
+  		)
+  	}
 
 }
